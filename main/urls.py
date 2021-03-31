2e531e13c2eece3ca2fb3420eedb4482761b43dd
markcherepakhapython <mvky2004@gmail.com>
2021-03-31 19:15:36 +0300

    Add registration

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
-from .views import index, other_page, profile
-from .views import BBLoginView, BBLogoutView, ChangeUserInfoView, BBPasswordChangeView
+from .views import index, other_page, profile, user_activate
+from .views import (
+    BBLoginView, 
+    BBLogoutView, 
+    ChangeUserInfoView, 
+    BBPasswordChangeView, 
+    RegisterUserView,
+    RegisterDoneView
+)
 
 app_name = 'main'
 urlpatterns = [
@@ -10,6 +17,9 @@ urlpatterns = [
     path('accounts/password/change', BBPasswordChangeView.as_view(), name='password_change'),
     path('accounts/profile/change', ChangeUserInfoView.as_view(), name='profile_change'),
     path('accounts/profile/', profile, name='profile'),
+    path('accounts/register/activate/<str:sign>/', user_activate, name = 'register_activate'),
+    path('accounts/register/done/', RegisterDoneView.as_view(), name='register_done'),
+    path('accounts/register/', RegisterUserView.as_view(), name='register'),
     path('<str:page>/', other_page, name='other'),
     path('', index, name='index')
 ]
